a0b621699269f84d18041986a06d98ad1f0dda59
Dennis Kim <ddkim4@gmail.com>
2020-09-03 17:28:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Jack"} lastName={"Jackson"} age={"23"} hairColor={"blonde"}/> 
-      <PersonCard firstName={"mary"} lastName={"Smith"} age={"5"} hairColor={"blue"}/>
-      <PersonCard firstName={"Jack"} lastName={"Jackson"} age={"23"} hairColor={"blonde"}/>
+      <PersonCard firstName={"Jack"} lastName={"Jackson"} age={23} hairColor={"blonde"}/> 
+      <PersonCard firstName={"mary"} lastName={"Smith"} age={5} hairColor={"blue"}/>
+      <PersonCard firstName={"Bob"} lastName={"Bobbers"} age={23} hairColor={"red"}/>
+      
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
-    render(){
-        return(
-            
+    constructor(props) {  ///pure js stuff
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    
+    render(){ //this is where the html stuff goes
+
+        //props doesn't change, state changes
+        return(            
                 <fieldset>
-                <div>
-                    <h1>My name is { this.props.firstName } { this.props.lastName }</h1>
-                    <h1>My age is { this.props.age }</h1>
+                <div> 
+                    <h1>My name is { this.props.firstName } { this.props.lastName }</h1> 
+                    <h1>My age is { this.state.age }</h1>
                     <h1>My hair color is { this.props.hairColor}</h1>
+
+                    <button onClick={ () => { this.setState({ age: this.state.age +1 }) }}>Increment Age</button>
+
                 </div>
                 </fieldset>
 
@@ -17,3 +28,4 @@ class PersonCard extends Component{
 }
 
 export default PersonCard;
+
